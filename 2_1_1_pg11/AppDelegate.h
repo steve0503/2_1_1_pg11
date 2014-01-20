7e09b5422b4ef5a1bedc9e8213ad3323368f1b6f
steve0503 <leehr76@gmail.com>
2014-01-20 11:10:27 +0900

    Initial Commit

---
+++ 2_1_1_pg11/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  2_1_1_pg11
+//
+//  Created by SDT-1 on 2014. 1. 20..
+//  Copyright (c) 2014년 steve. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
